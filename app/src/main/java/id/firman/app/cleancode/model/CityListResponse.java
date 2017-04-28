09cab10a7e91549cc89e00f88f47504ea4f84cd2
FirmanMFK <firman.mfk@gmail.com>
2017-04-28 17:54:04 +0700

    add CityListResponse

--- app/src/main/java/id/firman/app/cleancode/model/CityListResponse.java
+++ app/src/main/java/id/firman/app/cleancode/model/CityListResponse.java
@@ -1,8 +1,48 @@
 package id.firman.app.cleancode.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Firman on 4/28/2017.
  */
 
 public class CityListResponse {
+
+    @SerializedName("data")
+    @Expose
+    private List<CityListData> data = new ArrayList<CityListData>();
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("status")
+    @Expose
+    private int status;
+
+    public List<CityListData> getData() {
+        return data;
+    }
+
+    public void setData(List<CityListData> data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }

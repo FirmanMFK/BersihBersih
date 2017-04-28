4123f7ee53c11a2a878d76178c88931274aa8b87
FirmanMFK <firman.mfk@gmail.com>
2017-04-28 18:53:32 +0700

    add NetworkError response

--- app/src/main/java/id/firman/app/cleancode/networking/Response.java
+++ app/src/main/java/id/firman/app/cleancode/networking/Response.java
@@ -1,8 +1,29 @@
 package id.firman.app.cleancode.networking;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Firman on 4/28/2017.
  */
 
 public class Response {
+    @SerializedName("status")
+    public String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @SuppressWarnings({"unused", "used by Retrofit"})
+    public Response() {
+
+    }
+
+    public Response(String status) {
+        this.status = status;
+    }
 }
